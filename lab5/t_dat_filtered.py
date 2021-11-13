2584fabff37499807c67c65d8de85d969721e887
DenKulakovv <Den.Kulakovv@yandex.ru>
2021-11-13 10:14:18 +0300

    feat: add filtered data storage

--- lab5/t_dat_filtered.py
+++ lab5/t_dat_filtered.py
@@ -4,6 +4,17 @@ from t_dat import TDat, FxType
 
 
 class TDatFiltered(TDat):
-    @property
-    def points(self) -> Dict[int, float]:
-        return self._points
+    def append(self, x: int, fx: FxType):
+        
+        keys = sorted(self.points.keys())
+
+        left = max(filter(lambda i: i < x, keys))
+        right = min(filter(lambda i: i > x, keys))
+
+        if fx(x) - fx(right) > (fx(self._x_max) - fx(self._x_min)) * len(keys) and fx(x) - fx(left) > (fx(self._x_max) - fx(self._x_min)) * len(keys):
+            return
+
+        if fx(right) - fx(x) > (fx(self._x_max) - fx(self._x_min)) * len(keys) and fx(left) - fx(x) > (fx(self._x_max) - fx(self._x_min)) * len(keys):
+            return
+
+        self.points[x] = fx(x)
